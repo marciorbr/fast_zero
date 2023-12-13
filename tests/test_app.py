73f4d20e0d383a9192a3ac0c5c4d6ffea307abe9
MARCIO RIBEIRO DE OLIVEIRA <marcio.oliveira@trt14.jus.br>
2023-12-13 11:42:53 -0400

    Configuração do projeto

---
+++ tests/test_app.py
@@ -0,0 +1,23 @@
+from fastapi.testclient import TestClient
+
+from fast_zero.app import app
+
+"""
+Fase 1 - Organizar (Arrange)
+Nesta primeira etapa, estamos preparando o ambiente para o teste. No exemplo, a linha com o comentário Arrange não é o teste em si, ela monta o ambiente para que o teste possa ser executado. Estamos configurando um client de testes para fazer a requisição ao app.
+
+Fase 2 - Agir (Act)
+Aqui é a etapa onde acontece a ação principal do teste, que consiste em chamar o Sistema Sob Teste (SUT). No nosso caso, o SUT é a rota /, e a ação é representada pela linha response = client.get('/'). Estamos exercitando a rota e armazenando sua resposta na variável response. É a fase em que o código de testes executa o código de produção que está sendo testado. Agir aqui significa interagir diretamente com a parte do sistema que queremos avaliar, para ver como ela se comporta.
+
+Fase 3 - Afirmar (Assert)
+Esta é a etapa de verificar se tudo correu como esperado. É fácil notar onde estamos fazendo a verificação, pois essa linha sempre tem a palavra reservada assert. A verificação é booleana, ou está correta ou não está. Por isso, um teste deve sempre incluir um assert para verificar se o comportamento esperado está correto.
+"""
+
+
+def test_root_deve_retornar_200_e_ola_mundo():
+    client = TestClient(app)   # Arrange
+
+    response = client.get('/')   # Act
+
+    assert response.status_code == 200   # Assert
+    assert response.json() == {'message': 'Olá Mundo!'}   # Assert
