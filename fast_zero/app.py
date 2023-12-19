a45efc12b3a7df8cc4067edf55a8bfa5d2fa9b0b
MARCIO RIBEIRO DE OLIVEIRA <marcio.oliveira@trt14.jus.br>
2023-12-19 09:01:47 -0400

    Criando rota para criação de usuários

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fast_zero.schemas import UserSchema, UserPublic
 
 app = FastAPI()
 
@@ -6,3 +7,8 @@ app = FastAPI()
 @app.get('/')
 def read_root():
     return {'message': 'Olá Mundo!'}
+
+
+@app.post('/users/', status_code=201, response_model=UserPublic)
+def create_user(user: UserSchema):
+    return user
