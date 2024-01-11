ff7201a185b5a131dc340115ec9e43ba8ace55a6
MARCIO RIBEIRO DE OLIVEIRA <marcio.oliveira@trt14.jus.br>
2024-01-11 12:22:22 -0400

    Adicionando feature de atualização do token de usuário

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -55,3 +55,35 @@ def test_token_wrong_password(client, user):
     )
     assert response.status_code == 400
     assert response.json() == {'detail': 'Incorrect email or password'}
+
+
+def test_refresh_token(client, user, token):
+    response = client.post(
+        '/auth/refresh_token',
+        headers={'Authorization': f'Bearer {token}'},
+    )
+
+    data = response.json()
+
+    assert response.status_code == 200
+    assert 'access_token' in data
+    assert 'token_type' in data
+    assert data['token_type'] == 'bearer'
+
+
+def test_token_expired_dont_refresh(client, user):
+    with freeze_time('2023-07-14 12:00:00'):
+        response = client.post(
+            '/auth/token',
+            data={'username': user.email, 'password': user.clean_password},
+        )
+        assert response.status_code == 200
+        token = response.json()['access_token']
+
+    with freeze_time('2023-07-14 12:31:00'):
+        response = client.post(
+            '/auth/refresh_token',
+            headers={'Authorization': f'Bearer {token}'},
+        )
+        assert response.status_code == 401
+        assert response.json() == {'detail': 'Could not validate credentials'}
