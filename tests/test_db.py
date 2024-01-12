0dffa0bbfbe852f8f8adbca807c227db04e3e732
MARCIO RIBEIRO DE OLIVEIRA <marcio.oliveira@trt14.jus.br>
2024-01-12 11:47:21 -0400

    Adicionando modulo para criação de tarefas 'todo'

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,6 +1,7 @@
 from sqlalchemy import select
+from sqlalchemy.orm import Session
 
-from fast_zero.models import User
+from fast_zero.models import Todo, User
 
 
 def test_create_user(session):
@@ -12,3 +13,20 @@ def test_create_user(session):
 
     # assert
     assert result == user
+
+
+def test_create_todo(session: Session, user: User):
+    todo = Todo(
+        title='Test Todo',
+        description='Test Desc',
+        state='draft',
+        user_id=user.id,
+    )
+
+    session.add(todo)
+    session.commit()
+    session.refresh(todo)
+
+    user = session.scalar(select(User).where(User.id == user.id))
+
+    assert todo in user.todos
