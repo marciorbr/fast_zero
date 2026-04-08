d2d2c00512ca572252666e86cadf20ef7907afe5
Márcio Ribeiro de Oliveira <marcio035@gmail.com>
2026-04-08 17:51:56 -0400

    Criando endpoint para criar usuario

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,9 +1,10 @@
 import logging
 import sys
+from http import HTTPStatus
 
 from fastapi import FastAPI, Request
 
-from fast_zero.schemas import Message
+from fast_zero.schemas import Message, UserPublic, UserSchema
 
 # Configurar logging para saída no console
 logging.basicConfig(
@@ -20,3 +21,10 @@ app = FastAPI()
 def read_root(request: Request):
     logger.info(f'Requisição GET / - {request._headers}')
     return {'message': 'Olá Mundo!'}
+
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    # breakpoint()  # Ponto de interrupção para depuração l entra q sai
+    logger.info(f'Usuario criado: {user.username} - {user.email} - {user.password}')
+    return user
