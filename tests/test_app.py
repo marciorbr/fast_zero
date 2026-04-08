26bb101d071339dd50af570915c7c41d7e7a4a13
Márcio Ribeiro de Oliveira <marcio035@gmail.com>
2026-04-08 18:19:45 -0400

    Criando testes

--- tests/test_app.py
+++ tests/test_app.py
@@ -12,3 +12,23 @@ def test_root_deve_retornar_ok_e_ola_mundo():
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá Mundo!'}
+
+
+def test_create_user():
+    client = TestClient(app)
+
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'alice',
+            'email': 'alice@example.com',
+            'password': 'testpassword',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'id': 1,
+        'username': 'alice',
+        'email': 'alice@example.com',
+    }
