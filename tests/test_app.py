d81905bb27f85fef4664bb6e44d4fcb1cc8df5ed
Márcio Ribeiro de Oliveira <marcio035@gmail.com>
2026-04-08 18:25:33 -0400

    Configurando fixture para os testes

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,12 +1,17 @@
 from http import HTTPStatus
 
+import pytest
 from fastapi.testclient import TestClient
 
 from fast_zero.app import app
 
 
-def test_root_deve_retornar_ok_e_ola_mundo():
-    client = TestClient(app)
+@pytest.fixture
+def client():
+    return TestClient(app)
+
+
+def test_root_deve_retornar_ok_e_ola_mundo(client):
 
     response = client.get('/')
 
@@ -14,8 +19,7 @@ def test_root_deve_retornar_ok_e_ola_mundo():
     assert response.json() == {'message': 'Olá Mundo!'}
 
 
-def test_create_user():
-    client = TestClient(app)
+def test_create_user(client):
 
     response = client.post(
         '/users/',
