588403eb02d02afe6ac074c77c83a3de650666f8
Márcio Ribeiro de Oliveira <marcio035@gmail.com>
2026-04-07 20:51:06 -0400

    Configurando logger

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,20 @@
-from fastapi import FastAPI
+import logging
+import sys
+
+from fastapi import FastAPI, Request
+
+# Configurar logging para saída no console
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    stream=sys.stdout,
+)
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
 
 @app.get('/')
-def read_root():
+def read_root(request: Request):
+    logger.info(f'Requisição GET / - {request._headers}')
     return {'message': 'Olá Mundo!'}
