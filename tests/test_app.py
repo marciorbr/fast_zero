d44fdf63f307690704dd9792abb13c7c4180eaed
Márcio Ribeiro de Oliveira <marcio035@gmail.com>
2026-04-08 18:28:05 -0400

    Configurando conftest

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,15 +1,5 @@
 from http import HTTPStatus
 
-import pytest
-from fastapi.testclient import TestClient
-
-from fast_zero.app import app
-
-
-@pytest.fixture
-def client():
-    return TestClient(app)
-
 
 def test_root_deve_retornar_ok_e_ola_mundo(client):
 
