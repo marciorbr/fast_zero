6db92fe5c9baea7a7eac906c269c7a37864449fd
Márcio Ribeiro de Oliveira <marcio035@gmail.com>
2026-04-19 18:10:24 -0400

    refact: Adicionando Annotated

--- fast_zero/routers/auth.py
+++ fast_zero/routers/auth.py
@@ -1,4 +1,5 @@
 from http import HTTPStatus
+from typing import Annotated
 
 from fastapi import APIRouter, Depends, HTTPException
 from fastapi.security import OAuth2PasswordRequestForm
@@ -12,11 +13,13 @@ from fast_zero.security import create_access_token, verify_password
 
 router = APIRouter(prefix='/auth', tags=['auth'])
 
+T_Session = Annotated[Session, Depends(get_session)]
+
 
 @router.post('/token', response_model=Token)
 def login_for_access_token(
+    session: T_Session,
     form_data: OAuth2PasswordRequestForm = Depends(),
-    session: Session = Depends(get_session),
 ):
 
     user = session.scalar(select(User).where(User.email == form_data.username))
--- fast_zero/routers/users.py
+++ fast_zero/routers/users.py
@@ -1,5 +1,6 @@
 from http import HTTPStatus
 from sqlite3 import IntegrityError
+from typing import Annotated
 
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy import select
@@ -12,9 +13,12 @@ from fast_zero.security import get_current_user, get_password_hash
 
 router = APIRouter(prefix='/users', tags=['users'])
 
+T_Session = Annotated[Session, Depends(get_session)]
+T_CurrentUser = Annotated[User, Depends(get_current_user)]
+
 
 @router.post('/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
-def create_user(user: UserSchema, session: Session = Depends(get_session)):
+def create_user(user: UserSchema, session: T_Session):
 
     db_user = session.scalar(
         select(User).where(
@@ -48,10 +52,10 @@ def create_user(user: UserSchema, session: Session = Depends(get_session)):
 
 @router.get('/', status_code=HTTPStatus.OK, response_model=UserList)
 def read_users(
+    session: T_Session,
+    current_user: T_CurrentUser,
     limit: int = 10,
     offset: int = 0,
-    session: Session = Depends(get_session),
-    current_user: User = Depends(get_current_user),
 ):
 
     users = session.scalars(select(User).limit(limit).offset(offset))
@@ -62,8 +66,8 @@ def read_users(
 @router.get('/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic)
 def read_user_by_id(
     user_id: int,
-    session: Session = Depends(get_session),
-    current_user: User = Depends(get_current_user),
+    session: T_Session,
+    current_user: T_CurrentUser,
 ):
 
     user = session.scalar(select(User).where(User.id == user_id))
@@ -79,8 +83,8 @@ def read_user_by_id(
 def update_user(
     user_id: int,
     user: UserSchema,
-    session: Session = Depends(get_session),
-    current_user: User = Depends(get_current_user),
+    session: T_Session,
+    current_user: T_CurrentUser,
 ):
 
     if current_user.id != user_id:
@@ -110,8 +114,8 @@ def update_user(
 @router.delete('/{user_id}', response_model=Message)
 def delete_user(
     user_id: int,
-    session: Session = Depends(get_session),
-    current_user: User = Depends(get_current_user),
+    session: T_Session,
+    current_user: T_CurrentUser,
 ):
 
     if current_user.id != user_id:
