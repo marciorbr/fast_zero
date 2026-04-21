18a33fa18a155d6684c6182ae55b01607603b168
Márcio Ribeiro de Oliveira <marcio035@gmail.com>
2026-04-21 18:00:16 -0400

    vscode config

--- fast_zero/security.py
+++ fast_zero/security.py
@@ -15,14 +15,6 @@ from fast_zero.settings import Settings
 
 settings = Settings()
 
-# SECRET_KEY = settings.SECRET_KEY
-# ALGORITHM = settings.ALGORITHM
-# ACCESS_TOKEN_EXPIRE_MINUTES = settings.ACCESS_TOKEN_EXPIRE_MINUTES
-# TIME_ZONE = settings.TIME_ZONE
-
-# ACCESS_TOKEN_EXPIRE_MINUTES = 30
-# ALGORITHM = 'HS256'
-# TIME_ZONE = 'America/Porto_Velho'
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='auth/token')
 
