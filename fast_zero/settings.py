09e9bc1e9c3fe6173c8621a0e5e82e514cf77d1b
Márcio Ribeiro de Oliveira <marcio035@gmail.com>
2026-05-02 10:48:13 -0400

    feat: adiciona configuração de rede para o serviço de banco de dados no Docker Compose --- feat: atualiza a configuração do ambiente no Settings para ignorar variáveis vazias

--- fast_zero/settings.py
+++ fast_zero/settings.py
@@ -3,7 +3,10 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file='.env', env_file_encoding='utf-8'
+        env_file='.env', 
+        env_file_encoding='utf-8', 
+        extra="ignore",
+        env_ignore_empty=True,
     )
 
     DATABASE_URL: str
