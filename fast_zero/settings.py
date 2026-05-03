e30cc387db3e1d16c4ece668bec9212575543255
Márcio Ribeiro de Oliveira <marcio035@gmail.com>
2026-05-03 09:06:31 -0400

    style: ajusta formatação do arquivo settings.py

--- fast_zero/settings.py
+++ fast_zero/settings.py
@@ -3,9 +3,9 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file='.env', 
-        env_file_encoding='utf-8', 
-        extra="ignore",
+        env_file='.env',
+        env_file_encoding='utf-8',
+        extra='ignore',
         env_ignore_empty=True,
     )
 
